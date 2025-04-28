12dd13986e3734378df08dbf06972fc68f142730
Your Name <you@example.com>
2025-04-28 17:04:24 +0800

    first commit

---
+++ RandomNPCLoveEvent/RandomizedEvent.cs
@@ -0,0 +1,12 @@
+
+namespace RandomNPCLoveEvent
+{
+    class RandomizedEvent
+    {
+        public int OriginalHeartLevel; // 原版触发的好感度
+        public int RandomizedHeartLevel; // 随机后的好感度
+        public bool HasTriggered; // 是否已触发
+        public string EventID; // 事件标识
+    }
+
+}
